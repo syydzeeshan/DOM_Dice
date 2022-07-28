8c3c8f8a90889606f2694038d60abdeb109e084f
Zeeshan Ali Sayyad <75977170+syydzeeshan@users.noreply.github.com>
2022-07-28 21:12:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.random();
+randomNumber1 =Math.floor(randomNumber1*6)+1;
+var randomNumber2 = Math.random();
+randomNumber2 =Math.floor(randomNumber2*6)+1;
+
+ document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+
+
+ document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player 1 Wins🚩"
+} else if(randomNumber2>randomNumber1){
+  document.querySelector("h1").textContent="Player 2 Wins🚩"
+} else{
+  document.querySelector("h1").textContent="Draw"
+
+}
